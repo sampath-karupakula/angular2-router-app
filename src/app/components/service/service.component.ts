ea129fc1b42e0c28dd44b1a7cdd2ca2a6c9c557f
sampath-karupakula <karupakulasampath@gmail.com>
2017-01-26 14:51:36 +0530

    accessing path params

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-service',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceComponent implements OnInit {
 
-  constructor() { }
+  activeService: string;
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      this.activeService = params['active'];
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/components/services/services.route.cofing.ts
+++ src/app/components/services/services.route.cofing.ts
@@ -8,9 +8,7 @@ export const servicesRouteConfig: Routes = [
         path: 'service',
         children: [
             { path: '', component: ServicesComponent },
-            { path: 'service1', component: ServiceComponent },
-            { path: 'service2', component: ServiceComponent },
-            { path: 'service3', component: ServiceComponent },
+            { path: ':active', component: ServiceComponent },
         ]
     }
 ];
